199324df8399e63e9421f479a76810b7d2125ae1
w0219156 <w0219156@nscc.ca>
2024-04-06 16:35:34 -0300

    Largely working, some formatting to clean up

--- PROG2500-A2-Chinook/Pages/OrderPage.xaml.cs
+++ PROG2500-A2-Chinook/Pages/OrderPage.xaml.cs
@@ -1,28 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using PROG2500_A2_Chinook.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PROG2500_A2_Chinook.Pages
 {
-    /// <summary>
-    /// Interaction logic for OrderPage.xaml
-    /// </summary>
     public partial class OrderPage : Page
     {
+        private ChinookContext context = new ChinookContext();
+
         public OrderPage()
         {
             InitializeComponent();
+            LoadCustomerOrders();
+        }
+
+        private void LoadCustomerOrders(string search = "")
+        {
+            var customers = context.Customers
+                                   .Include(c => c.Invoices)
+                                   .ThenInclude(i => i.InvoiceLines)
+                                   .Where(c => string.IsNullOrEmpty(search) || (c.FirstName + " " + c.LastName).Contains(search))
+                                   .Select(c => new
+                                   {
+                                       FullName = c.LastName + ", " + c.FirstName,
+                                       Location = string.IsNullOrEmpty(c.State) ? $"{c.City}" : $"{c.City}, {c.State}",
+                                       c.Country,
+                                       c.Email,
+                                       Invoices = c.Invoices.Select(i => new
+                                       {
+                                           InvoiceDate = i.InvoiceDate,
+                                           TotalAmount = i.Total,
+                                           TrackCount = i.InvoiceLines.Count
+                                       }).ToList()
+                                   })
+                                   .ToList();
+
+            var collectionViewSource = (CollectionViewSource)this.FindResource("customersViewSource");
+            collectionViewSource.Source = customers;
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            LoadCustomerOrders(searchBox.Text);
         }
     }
 }
